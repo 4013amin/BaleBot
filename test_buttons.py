d07da2dd67ca4c668e229f2c40aef07e4ae67c27
delta <amin138400138400@gmail.com>
2026-05-04 19:22:18 +0330

    Change in Tamrin

--- test_buttons.py
+++ test_buttons.py
@@ -1,4 +1,4 @@
-from bale import Bot, Message, InlineKeyboardMarkup, InlineKeyboardButton
+from bale import Bot, Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQueryUpdate
 import sqlite3
 import asyncio
 
@@ -8,6 +8,18 @@ bot = Bot(token=BOT_TOKEN)
 dbName = "test.db"
 
 
+def showButton():
+    keyboard = InlineKeyboardMarkup()
+    
+    btn1 = InlineKeyboardButton("This is Button Testi", callback_data="btn1")
+    btn2 = InlineKeyboardButton("This is button Tow", callback_data="btn2")
+    
+    keyboard.add(btn1, row=1)
+    keyboard.add(btn2, row=2)
+    
+    return keyboard
+    
+
 @bot.event
 async def on_message(message: Message):
     if not message.content:
@@ -16,37 +28,20 @@ async def on_message(message: Message):
     content = message.content.strip().lower()
 
     if content == "/start":
-        inline_keyboard = InlineKeyboardMarkup()
-
-        inline_keyboard.add(
-            InlineKeyboardButton(text="✅ انجام شد", callback_data="done_1"),
-            row=1
-        )
-        inline_keyboard.add(
-            InlineKeyboardButton(text="🗑️حذف", callback_data="delete_1"),
-            row=1
-        )
-
-        # اضافه کردن دکمه ردیف دوم
-        inline_keyboard.add(
-            InlineKeyboardButton(text="📋 لیست کامل", callback_data="list_all"),
-            row=2
-        )
-
+        
         await message.reply(
             "🎯 این یک پیام تستی با دکمه است:\n\nروی دکمه‌ها کلیک کنید.",
-            components=inline_keyboard
+            components=showButton()
         )
+        
 
-    # هندل کردن پاسخ دکمه‌ها
-    elif message.content and message.content.startswith(('done_1', 'delete_1', 'list_all')):
-        if message.content == "done_1":
-            await message.reply("👍 دکمه 'انجام شد' زده شد!")
-        elif message.content == "delete_1":
-            await message.reply("🗑️ دکمه 'حذف' زده شد!")
-        elif message.content == "list_all":
-            await message.reply("📋 در حال نمایش لیست...")
-
+@bot.event
+async def on_callback_query(query: CallbackQueryUpdate):
+    if query and query.data :
+        callable_data = query.data
+        
+        await bot.answer_callback_query(query.id , "دکمه کلیک شد ! ")
+        
 
 if __name__ == "__main__":
     print("Testing buttons...")
